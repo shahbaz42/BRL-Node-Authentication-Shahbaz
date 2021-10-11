df4a780e5c1d72e23d270f9aba2488666052d967
shahbaz ali <shahbaz_ali@outlook.in>
2021-10-11 23:17:37 +0530

    add mongoDB

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 //jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
-app.use(bodyParser.urlencoded({extended : true}));
+const DB ="mongodb+srv://" + process.env.mongo_username + ":" + process.env.mongo_password + "@cluster0.wggru.mongodb.net/users?retryWrites=true&w=majority";
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function(req, res){
-    res.send("Monkey");
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connection Sucessful");
+  })
+  .catch((err) => console.log(err));
+
+app.get("/", function (req, res) {
+  res.send("Monkey");
 });
 
-app.listen(8000, function(){
+app.listen(8000, function () {
   console.log("Server started at port 8000.");
 });
