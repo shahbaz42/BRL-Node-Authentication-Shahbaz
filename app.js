38eee26874ce43de0485c30af59e09374ee137c7
shahbaz ali <shahbaz_ali@outlook.in>
2021-10-12 18:30:30 +0530

    Level 4 Authentication Salting and hashing passwords using bcrypt

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
 //jshint esversion:6
 const express = require("express");
-const ejs = require("ejs")
+const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-require("dotenv").config();  // for using env variables 
-const md5 = require("md5"); // for hashing password
+require("dotenv").config(); // for using env variables
+const bcrypt = require("bcrypt"); //for hashing and salting passwords
+const saltRounds = 10;
 
 const app = express();
-app.set("view engine", "ejs")
-const DB ="mongodb+srv://" + process.env.mongo_username + ":" + process.env.mongo_password + "@cluster0.wggru.mongodb.net/users?retryWrites=true&w=majority";
+app.set("view engine", "ejs");
+const DB =
+  "mongodb+srv://" +
+  process.env.mongo_username +
+  ":" +
+  process.env.mongo_password +
+  "@cluster0.wggru.mongodb.net/users?retryWrites=true&w=majority";
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //connecting mongodb atlas
-mongoose.connect(DB).then(() => {
+mongoose
+  .connect(DB)
+  .then(() => {
     console.log("Connection Sucessful");
-}).catch((err) => console.log(err));
+  })
+  .catch((err) => console.log(err));
 
 // We need to update schema our schema cant be just a js object
-const userSchema = new mongoose.Schema ({
-    email : String,
-    password : String
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
 });
 
 const User = new mongoose.model("User", userSchema);
@@ -28,52 +37,58 @@ app.get("/", function (req, res) {
   res.send("Monkey");
 });
 
-app.get("/login", function(req, res){
-    res.render("login");
+app.get("/login", function (req, res) {
+  res.render("login");
 });
 
-app.get("/register", function(req, res){
-    res.render("register");
+app.get("/register", function (req, res) {
+  res.render("register");
 });
 
 //handling register req made at /register
-app.post("/register", function(req, res){
-    const newUser = new User({
-        email : req.body.username,
-        password : md5(req.body.password)
-    })
+app.post("/register", function (req, res) {
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    //for hashing and salting passwords.
 
-    newUser.save(function(err){
-        if(err) {
-            console.log(err);
-        } else {
-            res.render("secrets");
-        }
-    })  
+    const newUser = new User({
+      email: req.body.username,
+      password: hash,
+    });
+
+    newUser.save(function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
+  });
 });
 
 // Handling login request made at /login
-app.post("/login", function(req, res){
-    const username = req.body.username;
-    const password = md5(req.body.password);
-    //finding username in database
-    User.findOne({email: username}, function(err, result){
-        if(err){                       // Not finding any records isn't an error condition, we need to handle that separately in else
-            console.log(err);
-        } if (result){
-            if(result.password === password){
-                res.render("secrets");
-            } else{
-                res.send("incorrect Password")
-            }
-        } else{                       
-            res.send("Email Not Found")
+app.post("/login", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+  //finding username in database
+  User.findOne({ email: username }, function (err, result) {
+    if (err) {
+      // Not finding any records isn't an error condition, we need to handle that separately in else
+      console.log(err);
+    }
+    if (result) {
+      bcrypt.compare(password, result.password, function (err, result) {
+        //using bcrypt to compare passwords
+        if (result === true) {
+          res.render("secrets");
+        } else {
+          res.send("Incorrect password");
         }
-    });
-
+      });
+    } else {
+      res.send("Email Not Found");
+    }
+  });
 });
-
-
 
 app.listen(8000, function () {
   console.log("Server started at port 8000.");
